74b75ea07835eee7ecf54e9ef19dd830c0a5723d
LaishaCalalpa <lcalalpa@gmail.com>
2020-02-04 14:50:41 +0000

    add solution

--- even_word.py
+++ even_word.py
@@ -1,2 +1,23 @@
-def even_word():
-    pass
+"""
+P: Return the minimum number of letters removed that would make a string even
+1. Take in a word
+2. Make a dictionary to count the occurances of each letter
+3. Declare a variable to store the amount of letters to be removed
+4. If the the value of the letter is odd, add 1 to letters to be removed
+"""
+
+def even_word(word):
+    counter = {};
+    removed = 0;
+    for letter in word:
+        if letter in counter:
+            counter[letter] += 1
+        else:
+            counter[letter] = 1
+
+    for letter in counter:
+        if counter[letter] % 2 != 0:
+            removed += 1
+
+    return removed
+
